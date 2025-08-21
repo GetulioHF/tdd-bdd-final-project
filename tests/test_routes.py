26d56a24bc4ec8b40fda935f2003570c46986e53
GetulioHF <getulio@gfs.com.br>
2025-08-21 15:22:49 -0400

    Final

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -43,7 +43,6 @@ DATABASE_URI = os.getenv(
 )
 BASE_URL = "/products"
 
-
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -166,6 +165,15 @@ class TestProductRoutes(TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    def test_get_product(self):
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
 
     ######################################################################
     # Utility functions
